fa76186e23bd8f781fb419a0d9b91e5f70b89f0b
kagerer19 <kagerer19@gmail.com>
2023-10-04 10:04:46 +0200

    finish SQL ex-4

--- tables/queries.sql
+++ tables/queries.sql
@@ -1,141 +1,141 @@
-/* EX-3 */
+/* SQL Exercise-3 */
 
 /* 1. output of PLAYERNO, NAME of players born after 1960.  */
-select PLAYERNO
-from PLAYERS
-where YEAR_OF_BIRTH > 1960;
+SELECT PLAYERNO
+FROM PLAYERS
+WHERE YEAR_OF_BIRTH > 1960;
 
 /* 2. output of PLAYERNO, NAME and TOWN of all female players who do not reside in Strat- ford. */
-select PLAYERNO, NAME, TOWN
-from PLAYERS
-where SEX = 'F';
+SELECT PLAYERNO, NAME, TOWN
+FROM PLAYERS
+WHERE SEX = 'F';
 
 /* 3. output of player numbers of players who joined the club between 1970 and 1980. */
-select PLAYERNO
-from PLAYERS
-where YEAR_JOINED >= 1970
-  and YEAR_JOINED >= 1980;
+SELECT PLAYERNO
+FROM PLAYERS
+WHERE YEAR_JOINED >= 1970
+  AND YEAR_JOINED >= 1980;
 
 
 /* 4. output of PlayerId, Name, Year of Birth of players born in a leap year. */
-select PLAYERNO
-from PLAYERS
-where (MOD(YEAR_OF_BIRTH, 4) = 0 and MOD(YEAR_OF_BIRTH, 100) <> 0)
-   or MOD(YEAR_OF_BIRTH, 400) = 0;
+SELECT PLAYERNO
+FROM PLAYERS
+WHERE (MOD(YEAR_OF_BIRTH, 4) = 0 AND MOD(YEAR_OF_BIRTH, 100) <> 0)
+   OR MOD(YEAR_OF_BIRTH, 400) = 0;
 
 
 /*5. output of the penalty numbers of the penalties between 50,- and 100,-.*/
-select AMOUNT
-from PENALTIES
-where AMOUNT >= 50
-  and AMOUNT <= 100;
+SELECT AMOUNT
+FROM PENALTIES
+WHERE AMOUNT >= 50
+  AND AMOUNT <= 100;
 
 
 /*6. output of PlayerId, name of players who do not live in Stratford or Douglas.*/
-select PLAYERNO, NAME
-from PLAYERS
-where TOWN not in ('Stratford', 'Douglas');
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE TOWN NOT IN ('Stratford', 'Douglas');
 
 /*7. output of playerId and name of players whose name contains 'is'.*/
-select PLAYERNO, NAME
-from PLAYERS
-where NAME like '%is%';
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE NAME LIKE '%is%';
 
 /*8. output of all hobby players.*/
-select PLAYERNO, NAME
-from PLAYERS
-where LEAGUENO IS NULL;
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE LEAGUENO IS NULL;
 
 
 /* 9. output of those employees who receive more commission than salary. */
-select EMPNO, ENAME
-from EMP
-where COMM > SAL;
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE COMM > SAL;
 
 
 /* 10. output of all employees from department 30 whose salary is greater than or equal to 1500. */
-select EMPNO, ENAME, DEPTNO
-from EMP
-where DEPTNO = 30
+SELECT EMPNO, ENAME, DEPTNO
+FROM EMP
+WHERE DEPTNO = 30
   and SAL >= 1500;
 
 /* 11. output of all managers who do not belong to department 30. */
-select MGR
-from EMP
-where DEPTNO not in (30);
+SELECT MGR
+FROM EMP
+WHERE DEPTNO NOT IN (30);
 
 
 /* 12. output of all employees from department 10 who are neither managers nor clerical workers (CLERK). */
-select EMPNO, ENAME
-from EMP
-where DEPTNO = 10
-  and JOB not in ('Manager', 'Clerk');
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE DEPTNO = 10
+  AND JOB NOT IN ('Manager', 'Clerk');
 
 
 /* 13. output of all employees who earn between 1200,- and 1300,-. */
-select EMPNO, ENAME
-from EMP
-where SAL > 1200
-  and SAL < 1300;
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE SAL > 1200
+  AND SAL < 1300;
 
 
 /* 14. output all employees whose name is 5 characters long and begins with ALL. */
-select *
-from EMP
-where length(ENAME) = 5
-  and ENAME like '%ALL%';
+SELECT *
+FROM EMP
+WHERE length(ENAME) = 5
+  AND ENAME LIKE '%ALL%';
 
 
 /* 15. output the total salary (salary + commission) for each employee. */
-select EMPNO, ENAME, SAL + NVL(COMM, 0) as TOTAL_SALARY
-from EMP;
+SELECT EMPNO, ENAME, SAL + NVL(COMM, 0) as TOTAL_SALARY
+FROM EMP;
 
 
 /* 16. output all employees, whose commission is over 25% of the salary. */
-select *
-from EMP
-where COMM > 0.25 * SAL;
+SELECT *
+FROM EMP
+WHERE COMM > 0.25 * SAL;
 
 
 /* 17. searched is the average salary of all office employees. */
-select round(avg(SAL))
-from EMP;
+SELECT round(avg(SAL))
+FROM EMP;
 
 
 /* 18. searched is the number of employees who have received a commission. */
-select EMPNO, ENAME
-from EMP
-where SAL is not null;
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE SAL IS NOT NULL ;
 
 
 /* 19. wanted is the number of different jobs in department 30. */
-select count(DISTINCT JOB)
-from EMP
-where DEPTNO = 30;
+SELECT count(DISTINCT JOB)
+FROM EMP
+WHERE DEPTNO = 30;
 
 
 /* 20. wanted is the number of employees in department 30. */
-select COUNT(EMPNO)
-from EMP
-where DEPTNO = 30;
+SELECT COUNT(EMPNO)
+FROM EMP
+WHERE DEPTNO = 30;
 
 /* 21. output of employees hired between 4/1/81 and 15/4/81. */
-select *
-from EMP
-where HIREDATE > TO_DATE('4-JAN-1981', 'DD-MON-YYYY')
-  and HIREDATE < TO_DATE('15-APR-1981', 'DD-MON-YYYY');
+SELECT *
+FROM EMP
+WHERE HIREDATE > TO_DATE('4-JAN-1981', 'DD-MON-YYYY')
+  AND HIREDATE < TO_DATE('15-APR-1981', 'DD-MON-YYYY');
 
 
-/* EX-4 */
+/* SQL Exercise-4 */
 /* 1. output TEAMNO of the teams in which the player with the number 27 is not captain */
-select TEAMNO
-from TEAMS
-where PLAYERNO in (select PLAYERNO from PLAYERS where PLAYERNO not in 27);
+SELECT TEAMNO
+FROM TEAMS
+WHERE PLAYERNO IN (SELECT PLAYERNO FROM PLAYERS WHERE PLAYERNO NOT IN 27);
 
 /* 2. output of PLAYERNO, NAME and INITIALS of the players who have won at least one match */
-select PLAYERNO, NAME, INITIALS
-from PLAYERS
-where PLAYERNO in (select PLAYERNO from MATCHES where WON >= 1);
+SELECT PLAYERNO, NAME, INITIALS
+FROM PLAYERS
+WHERE PLAYERNO IN (SELECT PLAYERNO FROM MATCHES WHERE WON >= 1);
 
 /* 3. output of playerNo and name of the players who have received at least one penalty */
 SELECT PLAYERNO, NAME
@@ -147,17 +147,132 @@ WHERE PLAYERNO IN
        HAVING COUNT(PLAYERNO) > 1);
 
 /* 4. output of playerNo and name of the players, who have received at least one penalty over 50.*/
-select PLAYERNO, NAME
-from PLAYERS
-where PLAYERNO in (select PLAYERNO from PENALTIES where AMOUNT > 50);
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE PLAYERNO in (SELECT PLAYERNO FROM PENALTIES WHERE AMOUNT > 50);
 
 /* 5. output of PlayerNo and name of players born in the same year as R. Parmenter*/
-select PLAYERNO, NAME
-from PLAYERS
-where YEAR_OF_BIRTH in (select YEAR_OF_BIRTH from PLAYERS where YEAR_OF_BIRTH = 1963);
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE YEAR_OF_BIRTH IN (SELECT YEAR_OF_BIRTH FROM PLAYERS WHERE YEAR_OF_BIRTH = 1963);
 
 
 /* 6. output of playerNo and name of the oldest player from Stratford*/
-select PLAYERNO, NAME
-from PLAYERS
-where YEAR_OF_BIRTH in (select min(YEAR_OF_BIRTH) from PLAYERS where TOWN = 'Stratford');
+SELECT PLAYERNO, NAME
+FROM PLAYERS
+WHERE YEAR_OF_BIRTH IN (SELECT min(YEAR_OF_BIRTH) FROM PLAYERS WHERE TOWN = 'Stratford');
+
+/*   SQL Exercise 5   */
+
+/* 1. number of new players per year */
+SELECT YEAR_JOINED, COUNT(*) AS NUMBER_OF_PLAYERS
+FROM PLAYERS
+GROUP BY YEAR_JOINED
+ORDER BY YEAR_JOINED;
+
+/* 2. number and average amount of penalties per player */
+SELECT PLAYERNO,
+       COUNT(*) AS NUMBER_OF_PENALTIES,
+       ROUND(AVG(AMOUNT)) AS AVERAGE_AMOUNT
+FROM PENALTIES
+GROUP BY PLAYERNO
+ORDER BY PLAYERNO;
+
+
+/* 3. number of penalties for the years before 1983 */
+SELECT EXTRACT(YEAR FROM PEN_DATE) AS PENALTY_YEAR, COUNT(*) AS NUMBER_OF_PENS
+FROM PENALTIES
+WHERE EXTRACT(YEAR FROM PEN_DATE) < 1983
+GROUP BY EXTRACT(YEAR FROM PEN_DATE)
+ORDER BY PENALTY_YEAR;
+
+
+/* 4. in which cities live more than 4 players */
+SELECT TOWN
+FROM PLAYERS
+GROUP BY TOWN HAVING COUNT(*) > 4;
+
+
+/* 5. PLAYERNO of those players whose penalty total is over 150 */
+SELECT PLAYERNO
+FROM PENALTIES
+WHERE AMOUNT > 150;
+
+
+/* 6. NAME and INITIALS of those players who received more than one penalty */
+SELECT NAME, INITIALS
+FROM PLAYERS
+WHERE PLAYERNO IN
+      (SELECT PLAYERNO FROM PENALTIES GROUP BY PLAYERNO HAVING COUNT(*) > 1);
+
+
+/* 7. in which years there were exactly 2 penalties */
+SELECT EXTRACT(YEAR FROM PEN_DATE) AS PENALTY_YEAR, COUNT(*) AS NUMBER_OF_PENS
+FROM PENALTIES
+GROUP BY EXTRACT(YEAR FROM PEN_DATE)
+HAVING COUNT(*) = 2
+ORDER BY PENALTY_YEAR;
+
+
+/* 8. NAME and INITIALS of the players who received 2 or more penalties over $40 */
+SELECT NAME, INITIALS
+FROM PLAYERS
+WHERE PLAYERNO IN
+      (SELECT PLAYERNO FROM PENALTIES GROUP BY PLAYERNO HAVING COUNT(*) > 2 AND SUM(PENALTIES.AMOUNT) > 40);
+
+
+/* 9. NAME and INITIALS of the player with the highest penalty amount */
+
+
+
+
+/* 10. in which year there were the most penalties and how many were there */
+
+/* 11. For each occurnce of a players in teams, show the PLAYERNO, TEAMNO, "WON - LOST" (nicely formatted, for example "3 - 2") sorted by the sum of lost Matches of this player in this team. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
