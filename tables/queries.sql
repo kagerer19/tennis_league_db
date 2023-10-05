985264587d47f99527b8d39c03c0dc8fd1cab174
kagerer19 <kagerer19@gmail.com>
2023-10-05 13:24:17 +0200

    finish SQL ex-5

--- tables/queries.sql
+++ tables/queries.sql
@@ -294,3 +294,77 @@ SELECT ROUND(AVG(SAL))  AS AVG_SALARY,
 FROM EMP
 WHERE DEPTNO = 30;
 
+
+/* SQL Exercise 6 */
+/* 1-5 Tennis query */
+
+
+/* 1. NAME, INITIALS and number of sets won for each player */
+SELECT NAME,
+       INITIALS,
+       (SELECT SUM(WON)
+        FROM MATCHES
+        WHERE PLAYERS.PLAYERNO = MATCHES.PLAYERNO) AS SETS_WON
+FROM PLAYERS
+ORDER BY NAME, INITIALS;
+
+/* 2. NAME, PEN_DATE and AMOUNT sorted in descending order by AMOUNT */
+SELECT NAME, PEN_DATE, AMOUNT
+FROM PLAYERS,
+     PENALTIES
+WHERE PLAYERS.PLAYERNO = PENALTIES.PLAYERNO
+ORDER BY AMOUNT DESC;
+
+
+/* 3. TEAMNO, NAME (of the captain) per team */
+SELECT TEAMNO, NAME
+FROM PLAYERS,
+     TEAMS
+WHERE PLAYERS.PLAYERNO = TEAMS.PLAYERNO;
+
+
+/* 4. NAME (player name), WON, LOST of all won matches*/
+SELECT NAME, WON, LOST
+FROM PLAYERS,
+     MATCHES
+WHERE PLAYERS.PLAYERNO = MATCHES.PLAYERNO
+  AND MATCHES.WON > 0;
+
+/* 5. PLAYERNO, NAME and penalty amount for each team player. If a player has not yet received a penalty, it should still be issued. Sorting should be done in ascending order of penalty amount */
+SELECT NAME, AMOUNT, PLAYERS.PLAYERNO
+FROM PLAYERS,
+     PENALTIES
+WHERE PLAYERS.PLAYERNO = PENALTIES.PLAYERNO
+ORDER BY PLAYERNO DESC;
+
+
+/* 6-9 EmptDept query */
+
+
+/* 6. in which city does the employee Allen work? */
+SELECT DEPT.LOC
+FROM EMP,
+     DEPT
+WHERE EMP.DEPTNO = DEPT.DEPTNO
+  AND EMP.ENAME = 'ALLEN';
+
+
+/* 7. search for all employees who earn more than their supervisor */
+
+/* 8. output the number of hires in each year */
+SELECT EXTRACT(YEAR FROM HIREDATE) AS HIRE_YEAR, COUNT(*) AS NUMBER_OF_HIRES
+FROM EMP
+GROUP BY EXTRACT(YEAR FROM HIREDATE)
+ORDER BY HIRE_YEAR;
+
+
+/* 9. output all employees who have a job like an employee from CHICAGO. */
+SELECT ENAME, JOB
+FROM EMP
+WHERE JOB IN (SELECT DISTINCT JOB
+              FROM EMP
+              WHERE DEPTNO IN (SELECT DEPTNO
+                               FROM DEPT
+                               WHERE LOC = 'CHICAGO'));
+
+
